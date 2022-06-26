ac77183bf7b539c972b0dfb5e15ba4efb0b4536e
Artaz <noobazi1212@gmail.com>
2022-06-26 19:22:13 +0430

    backtracking function coded'

--- TaskOne/main.cpp
+++ TaskOne/main.cpp
@@ -5,6 +5,40 @@ int car_number = -1;
 int Size_Of_parking = 4;
 QVector <int> qv;
 using namespace std;
+void backtracking(QMap<int , pair<int,int>> cars,bool visited[16],int camera,int curr,int want,QVector<int> v)
+{
+    if (want == curr)
+    {
+        qv.clear();
+        for (auto ii = v.begin(); ii != v.end(); ii++)
+        {
+            qv.push_back(*ii);
+        }
+        return;
+    }
+    visited[curr - 1] = true;
+    if (promising(visited,cars,camera,{curr+1,curr+2},'N') == true)
+    {
+        v.push_back(car_number);
+        backtracking(cars,visited,camera,curr+2,want,v);
+    }
+    if (promising(visited,cars,camera,{curr-2,curr-1},'B') == true)
+    {
+        v.push_back(car_number);
+        backtracking(cars,visited,camera,curr-2,want,v);
+    }
+    if (promising(visited,cars,camera,{curr+Size_Of_parking,curr+(2*Size_Of_parking)},'N') == true)
+    {
+        v.push_back(car_number);
+        backtracking(cars,visited,camera,curr+(2*Size_Of_parking),want,v);
+    }
+    if (promising(visited,cars,camera,{curr-(2*Size_Of_parking),curr-(Size_Of_parking)},'B') == true)
+    {
+        v.push_back(car_number);
+        backtracking(cars,visited,camera,curr-(2*Size_Of_parking),want,v);
+    }
+    return;
+}
 int main(void)
 {
     bool visited[16];
@@ -23,7 +57,7 @@ int main(void)
     int camera = 8;
     int start = 15,end = 7;
     QVector <int> v;
-    //backtracking(cars,visited,camera,start,end,v);
+    backtracking(cars,visited,camera,start,end,v);
     for (auto ii = qv.begin(); ii != qv.end(); ii++)
     {
         cout << *ii;
